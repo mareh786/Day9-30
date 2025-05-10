d320c2f556aea6b180e4ca1cc7928c8b32590163
Ubuntu <ubuntu@ip-172-31-14-222.ap-south-1.compute.internal>
2025-05-10 19:41:48 +0000

    Fixing app

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,10 @@ const port = 3000;
 
 app.use(cors());
 app.use(express.json());
-
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'password',
+  password: 'admin', 
   database: 'bookdb'
 });
 
